23a4fe5a39714e3c4920ef8969142e10626afcf3
qiaofeng1227 <76487013@qq.com>
2022-06-17 08:38:14 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-apt update
-apt install -y supervisor
+if [[ -f /usr/bin/supervisord ]]; then
+    echo "Initialization has been completed before this time!"
+else
+    apt update
+    apt install -y supervisor
+fi
+
 /usr/bin/supervisord
